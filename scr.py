045edce59be9a85ad91218bc40162e3fbe52c61a
Robrecht De Rouck <de.rouck.robrecht@gmail.com>
2014-11-08 02:29:16 +0100

    Implement function to measure prediction accuracy.

--- scr.py
+++ scr.py
@@ -299,6 +299,27 @@ gnb.fit(data_train, target_train)
 sk_gnb = sklearn.naive_bayes.GaussianNB()
 sk_gnb.fit(data_train, target_train)
 
+gnb_pred = gnb.predict(data_test)
+sk_gnb_pred = sk_gnb.predict(data_test)
 
-gnb.predict(data_test)
-sk_gnb.predict(data_test)
+
+def get_prediction_stats(predicted_targets, real_targets):
+    assert len(predicted_targets) == len(real_targets)
+    amount_of_targets = len(real_targets)
+    incorrectly_predicted = []
+    for predicted, real in zip(predicted_targets, real_targets):
+        if predicted != real:
+            incorrectly_predicted.append({"real": real, "predicted": predicted})
+
+    amount_incorrect = len(incorrectly_predicted)
+    amount_correct = amount_of_targets - amount_incorrect
+    accuracy = 1 - amount_incorrect/float(amount_of_targets)
+
+    print ("{0} out of {1} targets were predicted correctly "
+           "({2}% correct).".format(
+        amount_correct, amount_of_targets, accuracy * 100))
+    return accuracy, incorrectly_predicted, amount_of_targets
+
+gp = get_prediction_stats(gnb_pred, target_test)
+sk_gp = get_prediction_stats(sk_gnb_pred, target_test)
+
